7c89dc990f5cbcae9f1ee7ecb6e0b10be37e971d
Jacky <jackline.kimani@andela.com>
2016-09-06 14:49:36 +0300

    Remove dummy routes

--- server/routes/document.routes.js
+++ server/routes/document.routes.js
@@ -1,8 +1,1 @@
 'use strict';
-
-module.exports = function(app) {
-  app.route('/documents')
-    .get(function(req, res) {
-      res.json([{ name: 'Beverages' }, { name: 'Condiments' }]);
-    });
-};
