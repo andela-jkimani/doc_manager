7968d3266df7aadf39170a0693fb3f622a5739cd
Jacky <jackline.kimani@andela.com>
2016-09-14 22:22:44 +0300

    Add document routes

--- server/routes/document.routes.js
+++ server/routes/document.routes.js
@@ -1,1 +1,14 @@
-'use strict';
+var express = require('express');
+var app = express();
+var Document = require('../controllers/documents');
+
+app.route('/documents/')
+  .get(Document.getAll)
+  .post(Document.create);
+
+app.route('/documents/:id')
+  .get(Document.getOne)
+  .put(Document.update)
+  .delete(Document.delete);
+
+app.post('/document/delete', Document.delete);
